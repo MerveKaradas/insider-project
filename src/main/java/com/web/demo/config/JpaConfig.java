1094cb827a8b31180b4c75bf61811f7325aeaad7
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-07-19 14:49:57 +0300

    JPA config olusturuldu

---
+++ src/main/java/com/web/demo/config/JpaConfig.java
@@ -0,0 +1,12 @@
+package com.web.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+
+    // Otomatik kayıt tarihi ve güncelleme tarihi için gerekli ayarlar
+    
+}
