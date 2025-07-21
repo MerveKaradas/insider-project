cfb07b2d6ac5f411f97579d1897396fb77b4a08c
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-07-21 11:08:19 +0300

    Dosya guncellendi

--- src/main/java/com/web/demo/repository/abstracts/UserRepository.java
+++ src/main/java/com/web/demo/repository/abstracts/UserRepository.java
@@ -3,6 +3,7 @@ package com.web.demo.repository.abstracts;
 import com.web.demo.model.User;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,6 +13,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
     
 boolean existsByEmail(String email);
 List<User> findAll();
+Optional<User> findByEmail(String email);
    
     
 }
