7e0eda19e4edb1135bd5ad8505cfadcb955b40e9
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-07-21 11:07:51 +0300

    Yeni endpointler eklendi ve validasyon islemleri gerceklestirildi

--- src/main/java/com/web/demo/controller/UserManagementController.java
+++ src/main/java/com/web/demo/controller/UserManagementController.java
@@ -1,18 +1,25 @@
 package com.web.demo.controller;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.web.demo.dto.Request.LoginRequest;
 import com.web.demo.dto.Request.UserRequestDto;
 
 import com.web.demo.dto.Response.UserResponseDto;
+import com.web.demo.model.User;
+import com.web.demo.security.JwtUtil;
 import com.web.demo.service.abstracts.UserService;
 
 @RestController
@@ -20,13 +27,16 @@ import com.web.demo.service.abstracts.UserService;
 public class UserManagementController {
 
      private final UserService userService;
+     private final JwtUtil jwtUtil;
 
     // Constructor Injection
-    public UserManagementController(UserService userService) {
+    public UserManagementController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
+        this.jwtUtil = jwtUtil;
     }
     
     @GetMapping("/")
+    @PreAuthorize("hasRole('ADMIN')") // sadece admin erişimi
     public ResponseEntity<List<UserResponseDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
@@ -47,6 +57,14 @@ public class UserManagementController {
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
+    }
+
+    // Giriş yapan kullanıcıya token dönecek
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+        User user = userService.login(request.getEmail(), request.getPassword());
+        String token = jwtUtil.generateToken(user.getUsername(), user.getRole().name());
+        return ResponseEntity.ok(Collections.singletonMap("token", token));
     }
 
 }
