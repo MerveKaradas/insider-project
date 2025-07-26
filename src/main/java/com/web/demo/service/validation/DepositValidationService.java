96aeb3e401c509e59c483131c85ef4feac57c38b
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-07-26 22:46:58 +0300

    Deposit validasyon eklendi

--- src/main/java/com/web/demo/service/validation/DepositValidationService.java
+++ src/main/java/com/web/demo/service/validation/DepositValidationService.java
@@ -2,7 +2,23 @@ package com.web.demo.service.validation;
 
 import org.springframework.stereotype.Service;
 
+import com.web.demo.model.Balance;
+import com.web.demo.repository.abstracts.BalanceRepository;
+
 @Service
 public class DepositValidationService {
+
+     private final BalanceRepository balanceRepository;
+
+     
+
+     public DepositValidationService(BalanceRepository balanceRepository) {
+        this.balanceRepository = balanceRepository;
+    }
+
+     public Balance validateAndGetUserBalance(Long userId, String errorMessageIfNotFound) {
+        return balanceRepository.findBalanceByUserId(userId)
+                .orElseThrow(() -> new IllegalArgumentException(errorMessageIfNotFound));
+    }
     
 }
--- src/main/java/com/web/demo/strategy/DepositStrategy.java
+++ src/main/java/com/web/demo/strategy/DepositStrategy.java
@@ -6,12 +6,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.web.demo.repository.abstracts.BalanceRepository;
 import com.web.demo.service.abstracts.UserService;
+import com.web.demo.service.validation.DepositValidationService;
 import com.web.demo.model.User;
 import com.web.demo.model.Transaction;
 import com.web.demo.model.Balance;
 import com.web.demo.model.TransactionType;
 import com.web.demo.repository.abstracts.TransactionRepository;
-import com.web.demo.repository.abstracts.UserRepository;
 import com.web.demo.model.TransactionStatus; 
 import java.math.BigDecimal;
 
@@ -21,44 +21,68 @@ public class DepositStrategy implements TransactionStrategy {
     private final BalanceRepository balanceRepository;
     private final TransactionRepository transactionRepository;
     private final UserService userService;
-    private final UserRepository userRepository;
+    private final DepositValidationService validationService;
 
     public DepositStrategy(BalanceRepository balanceRepository,
                            TransactionRepository transactionRepository,
                            UserService userService,
-                           UserRepository userRepository) {
+                           DepositValidationService validationService) {
         this.balanceRepository = balanceRepository;
         this.transactionRepository = transactionRepository;
         this.userService = userService;
-        this.userRepository = userRepository;
+        this.validationService = validationService;
     }
 
     @Transactional
     @Override
     public Transaction processTransaction(TransactionRequestDto request) {
 
-        // Kullanıcıyı bul
-        User user = userService.findById(request.getFromUserId());
+        // Kullanıcı bulma
+        User user = userService.findById(request.getFromUserId()); 
+        User systemUser = userService.findByUsername("system");
+        
+         // Mevcut bakiyeyi alma
+        Balance balance = validationService.validateAndGetUserBalance(
+            user.getId(), "Kullanıcı bakiyesi bulunamadı");
 
-        // Mevcut bakiyeyi al
-        Balance balance = balanceRepository.findBalanceByUserId(user.getId())
-                .orElseThrow(() -> new IllegalArgumentException("Kullanıcı bakiyesi bulunamadı"));
+        try {
 
-        // Yeni bakiyeyi hesapla
-        BigDecimal newBalance = balance.getBalancesAmount().add(request.getTransactionAmount());
-        balance.setBalancesAmount(newBalance);
-        balanceRepository.save(balance); 
+            // Yeni bakiyeyi hesapla
+            BigDecimal newBalance = balance.getBalancesAmount().add(request.getTransactionAmount());
+            balance.setBalancesAmount(newBalance);
+            balanceRepository.save(balance); 
 
-        User systemUser = userRepository.findByUsername("system").orElseThrow();
-   
-        // Transaction oluştur ve kaydet
-        Transaction transaction = new Transaction();
-        transaction.setFromUserId(systemUser); 
-        transaction.setToUserId(user);
-        transaction.setTransactionAmount(request.getTransactionAmount());
-        transaction.setType(TransactionType.DEPOSIT);
-        transaction.setStatus(TransactionStatus.SUCCESS);
+            // Transaction oluştur ve kaydet
+            Transaction transaction = new Transaction();
+            transaction.setFromUserId(systemUser); 
+            transaction.setToUserId(user);
+            transaction.setTransactionAmount(request.getTransactionAmount());
+            transaction.setType(TransactionType.DEPOSIT);
+            transaction.setStatus(TransactionStatus.SUCCESS);
 
-        return transactionRepository.save(transaction); 
+            return transactionRepository.save(transaction); 
+
+        } catch(Exception e) {
+            // Başarısız işlem
+            Transaction failedTransaction = new Transaction();
+            failedTransaction.setFromUserId(systemUser); 
+            failedTransaction.setToUserId(user);
+            failedTransaction.setTransactionAmount(request.getTransactionAmount());
+            failedTransaction.setType(TransactionType.DEPOSIT);
+            failedTransaction.setStatus(TransactionStatus.FAILED);
+
+            transactionRepository.save(failedTransaction); 
+
+            
+           if (e instanceof IllegalArgumentException) throw e;
+
+            e.printStackTrace(); // TODO : loglama 
+            throw new RuntimeException("Para yatırma işlemi sırasında beklenmedik bir hata oluştu");
+            
+        }
+ 
+            
+
+        
     }
 }
