0ee33d54af9e504d139a5f8794d1d7bdff7255eb
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-08-18 22:01:49 +0300

    User update islemleri icin uygun metotlar tanimlandi

--- src/main/java/com/web/demo/service/abstracts/UserService.java
+++ src/main/java/com/web/demo/service/abstracts/UserService.java
@@ -1,9 +1,18 @@
 package com.web.demo.service.abstracts;
 
 import com.web.demo.dto.Request.UserRequestDto;
+import com.web.demo.dto.Request.UserUpdateEmailRequestDto;
+import com.web.demo.dto.Request.UserUpdatePasswordRequestDto;
+import com.web.demo.dto.Request.UserUpdateRequestDto;
+import com.web.demo.dto.Request.UserUpdateUsernameRequestDto;
 import com.web.demo.dto.Response.UserResponseDto;
 import com.web.demo.model.User;
+
+import jakarta.validation.Valid;
+
 import java.util.List;
+
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserService {
 
@@ -12,7 +21,7 @@ UserResponseDto register(UserRequestDto requestDto);
 
     UserResponseDto getUserById(Long id);
 
-    UserResponseDto updateUser(Long id, UserRequestDto requestDto);
+  //  UserResponseDto updateUser(Long id, UserRequestDto requestDto);
 
     void deleteUser(Long id);
 
@@ -20,6 +29,10 @@ UserResponseDto register(UserRequestDto requestDto);
     
     User findById(Long fromUserId);
     User findByUsername(String username);
+    UserResponseDto updateUser(UserDetails userDetails, UserUpdateRequestDto requestDto);
+    void updatePassword(Long userId, UserUpdatePasswordRequestDto requestDto);
+    void updateEmail(Long userId, UserUpdateEmailRequestDto requestDto);
+    void updateUsername(Long userId, UserUpdateUsernameRequestDto requestDto);
 
 
 
