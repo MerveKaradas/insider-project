541764a9b50fefbb84b33e21a6d21fa3d9fa9650
Merve Karadaş <100419329+MerveKaradas@users.noreply.github.com>
2025-08-18 22:03:10 +0300

    User endpointleri duzenlendi

--- src/main/java/com/web/demo/controller/UserController.java
+++ src/main/java/com/web/demo/controller/UserController.java
@@ -4,19 +4,28 @@ import java.util.Collections;
 import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.web.demo.dto.Request.LoginRequestDto;
 import com.web.demo.dto.Request.UserRequestDto;
+import com.web.demo.dto.Request.UserUpdateEmailRequestDto;
+import com.web.demo.dto.Request.UserUpdatePasswordRequestDto;
+import com.web.demo.dto.Request.UserUpdateRequestDto;
+import com.web.demo.dto.Request.UserUpdateUsernameRequestDto;
 import com.web.demo.dto.Response.UserResponseDto;
 import com.web.demo.event.AuditEventPublisher;
+import com.web.demo.model.User;
 import com.web.demo.service.abstracts.UserService;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -45,11 +54,47 @@ public class UserController {
         
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<UserResponseDto> updateUser(@PathVariable Long id,@RequestBody UserRequestDto requestDto) {
-        return ResponseEntity.ok(userService.updateUser(id, requestDto));
+    @PatchMapping("/me")
+    public ResponseEntity<UserResponseDto> updateUser(@RequestBody UserUpdateRequestDto requestDto, 
+                                                            @AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userService.updateUser(userDetails, requestDto));
 
     }
+
+    @PatchMapping("/me/password")
+    public ResponseEntity<Void> updateUserPassword(
+            @Valid @RequestBody UserUpdatePasswordRequestDto requestDto,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        Long userId = ((User) userDetails).getId();
+        userService.updatePassword(userId, requestDto);
+        return ResponseEntity.noContent().build(); // Başarılı olursa HTTP 204 (No Content)
+    }
+
+    @PatchMapping("/me/email")
+    public ResponseEntity<Void> updateUserEmail(
+            @Valid @RequestBody UserUpdateEmailRequestDto requestDto,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        Long userId = ((User) userDetails).getId();
+        userService.updateEmail(userId, requestDto);
+        return ResponseEntity.noContent().build(); // Başarılı olursa HTTP 204 (No Content)
+    }
+
+    @PatchMapping("/me/username")
+    public ResponseEntity<Void> updateUserUsername(
+            @Valid @RequestBody UserUpdateUsernameRequestDto requestDto,
+            @AuthenticationPrincipal UserDetails userDetails) {
+
+        if (userDetails == null) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+        Long userId = ((User) userDetails).getId();
+        userService.updateUsername(userId, requestDto);
+        return ResponseEntity.noContent().build(); // Başarılı olursa HTTP 204 (No Content)
+    }
+
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
@@ -70,7 +115,7 @@ public class UserController {
 
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponseDto> register(@RequestBody UserRequestDto requestDto) {
+    public ResponseEntity<UserResponseDto> register(@RequestBody @Valid UserRequestDto requestDto) {
         UserResponseDto responseDto = userService.register(requestDto);
         
         // Audit log publishing
